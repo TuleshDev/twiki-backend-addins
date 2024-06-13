0d45fda208d827e7c4fd31ac4432db87f3824553
tulesh <tulesh.hub@gmail.com>
2024-06-13 17:21:05 +0500

    Moved the 'server/models/pages.js' model from the 'twiki' project

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const test = () => {
-  return "Hello from twiki-backend-addins"
+const getPage = require('./server/models/pages')
+
+const updateModels = (models, pagesBase, pageHelper) => {
+  let additionalModels = {
+    pages: getPage(pagesBase, pageHelper)
+  }
+
+  models = {
+    ...models,
+    ...additionalModels
+  }
+
+  return models
 }
 
-module.exports = test
+module.exports = updateModels
