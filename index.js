b813fd26c05b7624cd6ce28a6aba68718fc6d196
tulesh <tulesh.hub@gmail.com>
2024-06-21 15:42:48 +0500

    Moved the 'server/graph/schemas/page.graphql' schema from the 'twiki' project

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const fs = require('fs')
 const getPage = require('./server/models/pages')
 
 module.exports = {
+  getUpdatableGraphSchemas () {
+    let additionalSchemas = fs.readdirSync(`${__dirname}/server/graph/schemas`)
+    return additionalSchemas
+  },
+  updateGraphSchemas (typeDefs) {
+    let additionalSchemas = this.getUpdatableGraphSchemas()
+    additionalSchemas.forEach(schema => {
+      typeDefs.push(fs.readFileSync(`${__dirname}/server/graph/schemas/${schema}`, 'utf8'))
+    })
+
+    return typeDefs
+  },
   updateModels (models, pagesBase, pageHelper) {
     let additionalModels = {
       pages: getPage(pagesBase, pageHelper)
